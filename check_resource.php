943fd3bde2e56c374e04232bcec619c1648d1f25
tiredoy <tiredoy@gmail.com>
2018-09-13 23:05:27 +0800

    t

--- check_resource.php
+++ check_resource.php
@@ -1,5 +1,23 @@
 <?php
 
+class PrettyOutput {
+    const SEP_MIN = 3;
+    const SEP_MAX = 50;
+    public static function sep($len = 10) {
+        $len = intval($len);
+        $len = $len < self::SEP_MIN ? self::SEP_MIN : ($len > self::SEP_MAX ? self::SEP_MAX : $len);
+        echo PHP_EOL;
+        for ($i=0; $i<$len; ++$i) {
+            echo '-';
+        }
+        echo PHP_EOL;
+    }
+    public static function raw($result_arr) {
+        foreach ($result_arr as $str) {
+            echo $str.PHP_EOL;
+        }
+    }
+}
 
 class AutoPushStaticResource {
 
@@ -66,15 +84,16 @@ class AutoPushStaticResource {
     private function stopConcurrency() {
         $cmd = "ps aux|grep check_resource|grep -v grep|wc -l";
         exec($cmd, $count);
-        $count = intval($count);
-        echo $count;
+        $count = intval($count[0]);
         if($count>1) {
-            die('concurrency conflict');
+            die(PHP_EOL.'concurrency conflict'.PHP_EOL);
         }
     }
 
     public function main() {
         $this->stopConcurrency();
+        sleep(10);
+        die('quit normal');
         sleep(10);
         chdir($this->path);
         $this->gitAddAll();
@@ -87,6 +106,35 @@ class AutoPushStaticResource {
 
 }
 
+class gitOperation {
 
-$obj = new AutoPushStaticResource;
-$obj->main();
+    private $work_dir;
+
+    function __construct($work_dir=null)
+    {
+        if (empty($work_dir)) {
+            $this->work_dir = getcwd();
+        } else {
+            if(!is_dir($work_dir)) {
+                throw new Exception("{$workdir} is not a dir");
+            }
+            $this->work_dir = $work_dir;
+        }
+        chdir($this->work_dir);
+    }
+
+    public function gitStatus() {
+        echo getcwd();
+        $cmd = "git status";
+        exec($cmd, $status);
+        PrettyOutput::sep(50);
+        PrettyOutput::raw($status);
+    } 
+}
+
+
+//$obj = new AutoPushStaticResource;
+//$obj->main();
+
+$git_op = new GitOperation;
+$git_op->gitStatus();
